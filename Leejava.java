265c0d7e5a042ceb18ce4e608dd73e3c217680ac
choidonghyoun <103809228+choidonghyoun@users.noreply.github.com>
2022-04-16 14:53:13 +0900

    Add files via upload

---
+++ Leejava.java
@@ -0,0 +1,22 @@
+public static void main(String[] args) {
+	{
+      // 정수 : int, 실수(소수) : double, 문장 : String
+
+      System.out.println(i); // 출력 : 100
+      int num1;
+      int num1 = 100;
+      System.out.println 
+      System.out.println(d); // 출력 : 3.14
+      double = 3.14;
+      System.out.println(s); // 출력 : 안녕하세요
+      stringin 
+      System.out.println(i); // 출력 : 200
+      int num;
+      
+      System.out.println(d); // 출력 : 10.5
+
+      System.out.println(s); // 출력 : 반갑습니다.
+
+
+  }
+}
